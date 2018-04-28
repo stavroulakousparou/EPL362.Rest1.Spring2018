6ca94bee1b401216cc7982371758c83a4de73449
stavroulakousparou <31859321+stavroulakousparou@users.noreply.github.com>
2018-04-28 22:45:43 +0300

    Update

---
+++ HOM/OpinionsMonth.java
@@ -0,0 +1,217 @@
+package HOM;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import GUI.ConnectDB;
+import GUI.loginPage;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.awt.Font;
+import javax.swing.JScrollPane;
+
+/**
+ * Summary of legal opinions given each month.
+ * 
+ * */
+public class OpinionsMonth {
+
+	private JFrame frame;
+	private JTextField branch_txt;
+	private JTable table;
+	private String opinion = "";
+	private static Connection conn = null;
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					OpinionsMonth window = new OpinionsMonth();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public OpinionsMonth() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame, and show up the values for the 
+	 * branch that the user chosen.
+	 * 
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 589, 367);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JLabel label = new JLabel("Enter Branch Id");
+
+		branch_txt = new JTextField();
+		branch_txt.setColumns(10);
+
+		JButton button = new JButton("Refresh");
+		button.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				DefaultTableModel model = (DefaultTableModel) table.getModel();
+				model.setRowCount(0);
+				String branchID = branch_txt.getText();
+				
+				if(branchID.equals("")){
+					JOptionPane.showMessageDialog(frame, "Please enter a branch id!");
+					return;
+				}
+
+				ConnectDB connection = new ConnectDB();
+				conn = connection.getDBConnection();
+
+				// If connection Failed
+				if (conn == null) {
+					System.out.println("Connectio Failes");
+				}
+				
+				boolean flag = false;
+
+				String sqlOp = "SELECT * FROM Consultation WHERE BranchID='" + branchID + "'";
+				PreparedStatement state = null;
+
+				try {
+					state = conn.prepareStatement(sqlOp);
+					ResultSet res = state.executeQuery();
+
+					// Get the strings from the database
+					while (res.next()) {
+						opinion = res.getString("LegalOpinion");
+
+						if (opinion.length() == 0) {
+							flag = true;
+							JOptionPane.showMessageDialog(frame, "No results!");
+						} else if (opinion.length() != 0){
+							flag = true;
+							model.addRow(new Object[] { opinion });
+						}
+					}
+
+					// when the user enter wrong 
+					if(!flag) {
+						JOptionPane.showMessageDialog(frame, "WRONG branch id! Enter again!");
+					}
+
+				} catch (SQLException e1) {
+					e1.printStackTrace();
+				}
+
+			}
+		});
+
+		JButton goBack = new JButton("GoBack");
+		goBack.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				frame.setVisible(false);
+				HOMViewpoint f = new HOMViewpoint();
+				f.main(null);
+			}
+		});
+
+		JButton logOutBut = new JButton("LogOut");
+		logOutBut.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				frame.setVisible(false);
+				loginPage f = new loginPage();
+				f.main(null);
+			}
+		});
+		
+		JLabel lblNewLabel = new JLabel("Legal Opinions Per Month");
+		lblNewLabel.setFont(new Font("Arial", Font.BOLD, 16));
+		
+		JScrollPane scrollPane = new JScrollPane();
+		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(24)
+					.addComponent(label, GroupLayout.PREFERRED_SIZE, 88, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(branch_txt, GroupLayout.PREFERRED_SIZE, 86, GroupLayout.PREFERRED_SIZE)
+					.addGap(39)
+					.addComponent(button, GroupLayout.PREFERRED_SIZE, 86, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED, 97, Short.MAX_VALUE)
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING, false)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(logOutBut, GroupLayout.PREFERRED_SIZE, 93, GroupLayout.PREFERRED_SIZE)
+							.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(goBack, GroupLayout.PREFERRED_SIZE, 93, GroupLayout.PREFERRED_SIZE)
+							.addGap(56))))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(176)
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+						.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 176, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblNewLabel))
+					.addContainerGap(200, Short.MAX_VALUE))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblNewLabel)
+					.addGap(18)
+					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 97, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED, 55, Short.MAX_VALUE)
+					.addComponent(goBack)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(19)
+							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+								.addComponent(branch_txt, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+								.addComponent(label)
+								.addComponent(button)))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(35)
+							.addComponent(logOutBut)))
+					.addGap(47))
+		);
+		
+				table = new JTable();
+				scrollPane.setViewportView(table);
+				table.setModel(new DefaultTableModel(
+					new Object[][] {
+					},
+					new String[] {
+						"Legal Opinions"
+					}
+				));
+		frame.getContentPane().setLayout(groupLayout);
+	}
+	
+
+}
---
+++ HOM/RecomendationsMonth.java
@@ -0,0 +1,206 @@
+package HOM;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.table.DefaultTableModel;
+
+import GUI.ConnectDB;
+import GUI.loginPage;
+
+import javax.swing.JTable;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.swing.JScrollPane;
+
+/**
+ * Summary of the recommendation given each month
+ * 
+ */
+
+public class RecomendationsMonth {
+
+	private JFrame frame;
+	private JTextField branch_txt;
+	private JTable table;
+	private String recomm = "";
+	 
+	private static Connection conn = null;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					RecomendationsMonth window = new RecomendationsMonth();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public RecomendationsMonth() {
+		initialize();
+	}
+
+	/**
+	 * Initializes the contents of the frame. Takes the branch id and search in the
+	 * database for this id. Then shows up the recommendations for this branch. When
+	 * the user enter wrong id or the branch not exists, shows up a dialog box with
+	 * a message.
+	 * 
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 648, 386);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JLabel lblRecomendations = new JLabel("Recomendations Per Month");
+		lblRecomendations.setFont(new Font("Arial", Font.BOLD, 16));
+
+		JLabel label = new JLabel("Branch ID: ");
+
+		branch_txt = new JTextField();
+		branch_txt.setColumns(10);
+
+		JButton button = new JButton("Refresh");
+		button.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				DefaultTableModel model = (DefaultTableModel) table.getModel();
+				model.setRowCount(0);
+				String branchID = branch_txt.getText();
+				if (branchID.equals("")) {
+					JOptionPane.showMessageDialog(frame, "Please enter a branch id!");
+					return;
+				}
+
+				ConnectDB connection = new ConnectDB();
+				conn = connection.getDBConnection();
+
+				// If connection Failed
+				if (conn == null) {
+					System.out.println("Connectio Failes");
+				}
+
+				boolean flag = false;
+				
+				String sqlReco = "SELECT * FROM Consultation WHERE BranchID='" + branchID + "'";
+				PreparedStatement state = null;
+
+				try {
+					state = conn.prepareStatement(sqlReco);
+					ResultSet res = state.executeQuery();
+
+					
+					// Get the strings from the database
+					while (res.next()) {
+						
+						recomm = res.getString("Recommendation");
+
+						if (recomm.length() == 0) {
+							JOptionPane.showMessageDialog(frame, "No results!");
+							flag = true;
+						} else if (recomm.length() != 0){
+							flag = true;
+							model.addRow(new Object[] { recomm });
+						} 
+
+					}
+
+					// when the user enter wrong 
+					if(!flag) {
+						JOptionPane.showMessageDialog(frame, "WRONG branch id! Enter again!");
+					}
+					
+				} catch (SQLException e1) {
+					e1.printStackTrace();
+				}
+
+			}
+		});
+
+		// Go back button
+		JButton goBackBtn = new JButton("GoBack");
+		goBackBtn.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				frame.setVisible(false);
+				HOMViewpoint f = new HOMViewpoint();
+				f.main(null);
+			}
+		});
+
+		// Log out button
+		JButton logoutBtn = new JButton("LogOut");
+		logoutBtn.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				frame.setVisible(false);
+				loginPage lp = new loginPage();
+				lp.main(null);
+			}
+		});
+
+		JScrollPane scrollPane = new JScrollPane();
+		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
+		groupLayout.setHorizontalGroup(groupLayout.createParallelGroup(Alignment.TRAILING).addGroup(groupLayout
+				.createSequentialGroup().addGap(27)
+				.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup().addGap(467).addComponent(goBackBtn,
+								GroupLayout.PREFERRED_SIZE, 93, GroupLayout.PREFERRED_SIZE))
+						.addGroup(groupLayout.createSequentialGroup()
+								.addComponent(label, GroupLayout.PREFERRED_SIZE, 88, GroupLayout.PREFERRED_SIZE)
+								.addGap(18)
+								.addComponent(branch_txt, GroupLayout.PREFERRED_SIZE, 86, GroupLayout.PREFERRED_SIZE)
+								.addGap(57)
+								.addComponent(button, GroupLayout.PREFERRED_SIZE, 86, GroupLayout.PREFERRED_SIZE)
+								.addGap(132)
+								.addComponent(logoutBtn, GroupLayout.PREFERRED_SIZE, 93, GroupLayout.PREFERRED_SIZE)))
+				.addContainerGap(45, Short.MAX_VALUE))
+				.addGroup(groupLayout.createSequentialGroup().addContainerGap(214, Short.MAX_VALUE)
+						.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING, false)
+								.addComponent(scrollPane, Alignment.LEADING, 0, 0, Short.MAX_VALUE)
+								.addComponent(lblRecomendations, Alignment.LEADING, GroupLayout.DEFAULT_SIZE,
+										GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+						.addGap(206)));
+		groupLayout.setVerticalGroup(groupLayout.createParallelGroup(Alignment.LEADING).addGroup(groupLayout
+				.createSequentialGroup().addContainerGap().addComponent(lblRecomendations).addGap(18)
+				.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 132, GroupLayout.PREFERRED_SIZE)
+				.addPreferredGap(ComponentPlacement.RELATED, 51, Short.MAX_VALUE).addComponent(goBackBtn).addGap(1)
+				.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup().addGap(4).addComponent(label))
+						.addGroup(groupLayout.createSequentialGroup().addGap(1).addComponent(branch_txt,
+								GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addComponent(button)
+						.addGroup(groupLayout.createSequentialGroup().addGap(17).addComponent(logoutBtn)))
+				.addGap(52)));
+
+		table = new JTable();
+		scrollPane.setViewportView(table);
+		table.setModel(new DefaultTableModel(new Object[][] {}, new String[] { "Recommendations" }));
+		frame.getContentPane().setLayout(groupLayout);
+	}
+
+}
